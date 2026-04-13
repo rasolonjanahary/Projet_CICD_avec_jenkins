240b18944b4cab8c7713527df1c9532a15ebd860
rasolonjanahary <rasolonjanahary1263@gmail.com>
2026-04-13 10:24:15 +0200

    correction sur templates

--- src/app.py
+++ src/app.py
@@ -32,7 +32,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="index.html")
 
 @app.get("/api")
 def home():
