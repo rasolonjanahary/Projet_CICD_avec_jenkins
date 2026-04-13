4239d2cf6567c49c747c1b9137667eb669abee7f
rasolonjanahary <rasolonjanahary1263@gmail.com>
2026-04-13 14:06:47 +0200

    ajout de dataset sur mlflow

--- notebook/train.py
+++ notebook/train.py
@@ -9,6 +9,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.compose import ColumnTransformer
+from mlflow.data.pandas_dataset import from_pandas
 
 mlflow.set_experiment("wine")
 
@@ -51,35 +52,39 @@ model_svc = SVC()
 
 accuracie = []
 
+dataset = from_pandas(df, source="wine_quality.csv", name="wine_fraud_data")
+
 with mlflow.start_run():
+    mlflow.log_input(dataset, context="training")
+
     # Random forest classifier
     model_rfc.fit(X_train, y_train)
     acc = model_rfc.score(X_test, y_test)
     mlflow.log_metric("accuracy rfc", acc)
-    mlflow.sklearn.log_model(model_rfc, "model_rfc")
+    mlflow.sklearn.log_model(model_rfc, name="model_rfc")
     accuracie.append(acc)
 
     # Decision tree classifier
     model_dtc.fit(X_train, y_train)
     acc_dtc = model_dtc.score(X_test, y_test)
     mlflow.log_metric("accuracy dtc", acc_dtc)
-    mlflow.sklearn.log_model(model_dtc, "model_dtc")
+    mlflow.sklearn.log_model(model_dtc, name="model_dtc")
     accuracie.append(acc_dtc)
 
     # Logistic regression
     model_lr.fit(X_train, y_train)
     acc_lr = model_lr.score(X_test, y_test)
     mlflow.log_metric("accuracy lr", acc_lr)
-    mlflow.sklearn.log_model(model_lr, "model_lr")
+    mlflow.sklearn.log_model(model_lr, name="model_lr")
     accuracie.append(acc_lr)
 
     # svc
     model_svc.fit(X_train, y_train)
     acc_svc = model_svc.score(X_test, y_test)
     mlflow.log_metric("accuracy svc", acc_svc)
-    mlflow.sklearn.log_model(model_svc, "model_svc")
+    mlflow.sklearn.log_model(model_svc, name="model_svc")
     accuracie.append(acc_svc)
 
-    mlflow.sklearn.log_model(preprocessor, "preprocessor")
+    mlflow.sklearn.log_model(preprocessor, name="preprocessor")
 
     print("Accuracy:", accuracie)
